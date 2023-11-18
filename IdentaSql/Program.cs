93c1d275f226d0f9b92fcde1ca645d1873147582
Lucas Zottis <zottis.lucas@hotmail.com>
2023-11-18 14:30:46 -0300

    AJustes

--- IdentaSql/Program.cs
+++ IdentaSql/Program.cs
@@ -1,5 +1,4 @@
 ﻿using SqlFormatter;
-using System.Text.RegularExpressions;
 
 namespace IdentaSql;
 
@@ -7,11 +6,8 @@ internal class Program
 {
     static void Main( string[] args )
     {
-        string sql1 = @" SELECT id, nome FROM usuarios WHERE idade > 18";
-        string sql2 = @" SELECT id, nome FROM usuarios WHERE idade in ( 18, 6, 9 )";
-
-        Console.WriteLine( Formatter.Format( sql1 ) );
+        Console.WriteLine( Formatter.Format( "SELECT id, nome FROM usuarios WHERE idade > 18" ) );
         Console.WriteLine();
-        Console.WriteLine( Formatter.Format( sql2 ) );
+        Console.WriteLine( Formatter.Format( "SELECT id, nome FROM usuarios WHERE idade in ( 18, 6, 9 )" ) );
     }
 }
